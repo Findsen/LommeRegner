d8eaa7d8b9be4ddb69ecf0e698cc088ca146dc91
Findsen <chr.findsen@gmail.com>
2017-09-17 17:19:31 +0200

    firstcommit

---
+++ app/src/main/java/dk/kea/student/class2017/christianfindsen/lommeregner/MainActivity.java
@@ -0,0 +1,15 @@
+package dk.kea.student.class2017.christianfindsen.lommeregner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
